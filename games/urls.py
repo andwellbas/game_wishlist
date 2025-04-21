dfe3ad4b1716b19743534adf800b90e0787381bf
andwellbas <basanetand@gmail.com>
2025-04-21 14:59:42 +0300

    Update functionality

--- games/urls.py
+++ games/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.game_list, name="game_list"),
+    path("add/", views.add_game, name="add_game"),
+    path("delete/<int:game_id>/", views.delete_game, name="delete_game"),
+    path("update-status/<int:game_id>/", views.update_status, name="update_status"),
 ]
--- games/views.py
+++ games/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
-from .models import Game
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from .models import Game, STATUS_CHOICES
+from .forms import GameForm
 
-# Create your views here.
+
 def game_list(request):
-    games = Game.objects.all()
-    return render(request, "games/game_list.html", {"games": games})
+    status_filter = request.GET.get("status")
+    if status_filter:
+        games = Game.objects.filter(status=status_filter)
+    else:
+        games = Game.objects.all()
+
+    form = GameForm()  # for getting choices
+
+    return render(request, "games/game_list.html", {
+        "games": games,
+        "status_filter": status_filter,
+        "form": form
+    })
+
+
+def add_game(request):
+    if request.method == "POST":
+        form = GameForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("game_list")
+    else:
+        form = GameForm()
+    return render(request, "games/add_game.html", {"form": form})
+
+
+def delete_game(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+    game.delete()
+    return redirect("game_list")
+
+
+@require_POST
+def update_status(request, game_id):
+    new_status = request.POST.get("status")
+    game = get_object_or_404(Game, id=game_id)
+    if new_status in dict(STATUS_CHOICES).keys():
+        game.status = new_status
+        game.save()
+    return redirect("game_list")
